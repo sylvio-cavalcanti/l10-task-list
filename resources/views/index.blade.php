5cd3a1cfc08982fbeec0245ba25e0c9b4fd85a4f
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-07-15 10:41:57 -0300

    Using directives: @if, @foreach and @forelse

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,25 @@
-<div>Hello from blade</div>
+<h1>The list of tasks</h1>
 <br>
-@isset($name)
-    <div>Hello {{ $name }}</div>
-@endisset
+<div>
+    {{-- First Alternative using if, else and foreach --}}
+    @if (count($tasks))
+        @foreach ($tasks as $task)
+            <div>
+                {{ $task->title }}
+            </div>
+        @endforeach
+    @else
+        <div>There are no tasks</div>
+    @endif
+
+    <br><br>
+    
+    {{-- Second Alternative using forelse which does the same thing --}}
+    @forelse ($tasks as $task)
+        <div>
+            {{ $task->title }}
+        </div>
+    @empty
+        <div>There are no tasks</div>
+    @endforelse
+</div>
