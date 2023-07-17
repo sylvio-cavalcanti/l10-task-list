917e23c48588a1e59119f148fd99b6239c04ff31
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-07-17 15:11:38 -0300

    Factory and Seeder (loading fake data into the DB)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create(); // Specifies how many models to be generated
+        \App\Models\User::factory(2)->unverified()->create(); // Custom 
+        \App\Models\Task::factory(20)->create(); // creates 20 tasks
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
